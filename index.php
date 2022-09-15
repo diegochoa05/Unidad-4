df0347a31080b7f5f9e8e5f75ea4307f60501458
Diego Ochoa <diegochoa0510@gmail.com>
2022-09-15 10:02:21 -0600

    Se agregaron estilos en el css

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Acceso al panel</title>
+    <link rel="stylesheet" href="public/css/main.css" >
+</head>
+<body>
+
+<div class = "container">
+
+<form>
+    <fieldset>
+        <legend>
+            Datos de acceso
+        </legend>
+
+        <label>
+            Email:
+        </label>
+
+        <input type="text" name="" placeholder="write here">
+
+        <label>
+            Password 
+        </label>
+
+        <input type="password" name="" placeholder="* * * * * * *">
+
+        <button type="submit">
+            Acceder
+        </button>
+    </fieldset>
+</form>
+
+
+</div>
+    
+</body>
+</html>
