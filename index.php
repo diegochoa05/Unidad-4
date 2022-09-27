c196f7119ad949a32e5d5283f91ef2f20061be84
Diego Ochoa <diegochoa0510@gmail.com>
2022-09-27 16:36:09 -0600

    Se termino los detalles de las cards y se separó la plantilla por secciones

--- index.php
+++ index.php
@@ -8,43 +8,9 @@
     <link rel="stylesheet" href="public/css/main.css" >
     <!-- CSS only -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <script src="https://cdn.tailwindcss.com"></script>
-
-    <style >
-      div{
-        
-      }
-    </style>
 </head>
 <body>
-    
-
-    <!-- <div class="flex min-h-full items-center justify-center">
-        
-
-        <div class="item-class"> 
-            <br><br>
-            <img class="mx-auto h-20 w-auto" src="https://www.uabcs.mx/assets/images/tail-assets/logo_uabcs.png" alt="Your Company">
-            <br>
-            <h1 class="text-center" >
-                Acceso
-            </h1>
-            <br><br>
-
-            <div class="mb-3">
-                <label for="exampleInputEmail1" class="form-label text-center">Correo</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
-            </div>
-
-            <div class="mb-3">
-                <label for="exampleInputPassword1" class="form-label text-center">Contraseña</label>
-                <input type="password" class="form-control" id="exampleInputPassword1">
-            </div>
-
-            <button type="button" class="btn btn-outline-primary">Entrar</button>
-        </div>
-
-    </div> -->
+  
 
     <div class="container">
       <section>
@@ -90,7 +56,6 @@
 
     </div>
     <!-- JavaScript Bundle with Popper -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
 
 </body>
 </html>
--- product/detalles.php
+++ product/detalles.php
@@ -1,97 +1,29 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Productos</title>
-    <!-- CSS only -->
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
+  <title>Productos</title>
+  <?php
+    include "../layouts/head.php";
+  ?>
 </head>
 <body>
 
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <a class="navbar-brand" href="#">
-      <img src="../public/img/meme.png" width="30" height="30" alt="">
-    </a>
-  <a class="navbar-brand" href="#">Navbar</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-    <span class="navbar-toggler-icon"></span>
-  </button>
+      <?php
+        include "../layouts/header.php";
+      ?>
 
-  <div class="collapse navbar-collapse" id="navbarSupportedContent">
-    <ul class="navbar-nav mr-auto">
-      <li class="nav-item active">
-        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#">Link</a>
-      </li>
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-          Dropdown
-        </a>
-        <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-          <a class="dropdown-item" href="#">Action</a>
-          <a class="dropdown-item" href="#">Another action</a>
-          <div class="dropdown-divider"></div>
-          <a class="dropdown-item" href="#">Something else here</a>
+      <div class="row">
+
+        <div class="col-2">
+          <!-- SIDEBAR -->
+
+          <?php
+            include "../layouts/sidebar.php";
+          ?>
+
         </div>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#">Disabled</a>
-      </li>
-    </ul>
-    <form class="form-inline my-2 my-lg-0">
-      <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-      <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-  </div>
-</nav>
 
-<div class="wrapper">
-    <!-- Sidebar -->
-    <nav id="sidebar">
-       
-
-        <ul class="list-unstyled components">
-            <p>Dummy Heading</p>
-            <li class="active">
-            <a href="#">Home</a>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false" class="dropdown-toggle">Pages</a>
-                <ul class="collapse list-unstyled" id="pageSubmenu">
-                    <li>
-                        <a href="#">Page 1</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 2</a>
-                    </li>
-                    <li>
-                        <a href="#">Page 3</a>
-                    </li>
-                </ul>
-            </li>
-            <li>
-                <a href="#">Portfolio</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-        </ul>
-
-        
-    </nav>
-
-    
-
-</div>
-
-          <div class="col-lg-10 col-sm-12">
+        <div class="col-lg-10 col-sm-12">
                 <div class="container-fluid">
                     <div class="border-buttom">
                         <div class="row m-2">
@@ -106,24 +38,30 @@
                         </div>
                     </div>
                     <div class="row">
-                        <?php for ($i=0; $i < 10; $i++): ?>
+                        
 
-                            <div class="col-md-3 col-sm-12 p-2">
-                            <div class="card" style="width: 18rem;">
-                              <img src="../public/img/meme.png" class="card-img-top" alt="...">
-                              <div class="card-body">
-                                <h5 class="card-title text-center">Card title</h5>
-                                <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                                <a href="#" class="btn btn-primary">Editar</a>
-                                <a href="#" class="btn btn-primary" on>Eliminar</a>
-                                <a href="#" class="btn btn-primary">Detalles</a>
-                              </div>
+                        <div class="col-md-3 col-sm-12 p-2">
+                        <div class="card" style="width: 18rem;">
+                          <img src="../public/img/meme.png" class="card-img-top" alt="...">
+                          <div class="card-body">
+                            <h5 class="card-title text-center">Card title</h5>
+                            <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+
+                            <div class="text-center">
+                              <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createProductModal">Editar</a>
+                              <a href="#" class="btn btn-primary" onclick="remove()">Eliminar</a>
                             </div>
-                            </div>
-                        <?php endfor ?>
+                            
+                          </div>
+                        </div>
+                        </div>
+                        
                     </div>
                 </div>
             </div>
+
+      </div>
+          
         </div>
 
         <!-- Modal -->
@@ -136,15 +74,14 @@
             </div>
             <div class="modal-body">
 
-    
+            <?php for ($i=0; $i < 6; $i++): ?>
 
               <div class="input-group mb-3">
-                <span class="input-group-text" id="basic-addon1">@</span>
-                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
-                
-              </div>
+              <span class="input-group-text" id="basic-addon1">@</span>
+              <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
             
-            
+            <?php endfor ?>
               
             </div>
             <div class="modal-footer">
@@ -155,27 +92,30 @@
         </div>
       </div>
 
-      <script type="text/javascript">
+      <script>
         function remove(){
           swal({
-          title: "Are you sure?",
-          text: "Once deleted, you will not be able to recover this imaginary file!",
-          icon: "warning",
-          buttons: true,
-          dangerMode: true,
-        })
-        .then((willDelete) => {
-          if (willDelete) {
-            swal("Poof! Your imaginary file has been deleted!", {
-              icon: "success",
-            });
-          } else {
-            swal("Your imaginary file is safe!");
-          }
-        });
+            title: "Are you sure?",
+            text: "Once deleted, you will not be able to recover this imaginary file!",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+          })
+          .then((willDelete) => {
+            if (willDelete) {
+              swal("Poof! Your imaginary file has been deleted!", {
+                icon: "success",
+              });
+            } else {
+              swal("Your imaginary file is safe!");
+            }
+          });
         }
+      </script>
 
-
+  <?php
+    include "../layouts/script.php";
+  ?>
 
 
 <!-- JavaScript Bundle with Popper -->
