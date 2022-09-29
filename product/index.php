1fb1c1dc141a16956d4103db25ce181d6a16c1fa
Diego Ochoa <diegochoa0510@gmail.com>
2022-09-28 23:09:29 -0600

    Se agregaron los productos de la api

--- product/index.php
+++ product/index.php
@@ -1,3 +1,10 @@
+<?php
+
+  include "../app/ProductCont.php";
+  $prod = new ProductCont();
+  $products = $prod->productsApi();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -42,10 +49,10 @@
 
                             <div class="col-md-3 col-sm-12 p-2">
                             <div class="card" style="width: 18rem;">
-                              <img src="../public/img/meme.png" class="card-img-top" alt="...">
+                              <img src="<?php echo $products[$i]->cover?>" class="card-img-top" alt="...">
                               <div class="card-body">
-                                <h5 class="card-title text-center">Card title</h5>
-                                <p class="card-text text-center">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                                <h5 class="card-title text-center"><?php echo $products[$i]->name ?></h5>
+                                <p class="card-text text-center"><?php echo $products[$i]->description ?></p>
                                 <a href="#" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#createProductModal">Editar</a>
                                 <a href="#" class="btn btn-primary" onclick="remove()">Eliminar</a>
                                 <a href="detalles.php" class="btn btn-primary" >Detalles</a>
